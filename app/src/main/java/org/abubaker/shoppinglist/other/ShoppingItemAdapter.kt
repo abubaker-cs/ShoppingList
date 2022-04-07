fff06d18f774c6e7901423e9ce2f5e495b8d1131
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-04-07 18:22:11 +0500

    Updated issues in ShoppingItemAdapter.kt by implementing bindingView

--- app/src/main/java/org/abubaker/shoppinglist/other/ShoppingItemAdapter.kt
+++ app/src/main/java/org/abubaker/shoppinglist/other/ShoppingItemAdapter.kt
@@ -6,40 +6,78 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.abubaker.shoppinglist.R
 import org.abubaker.shoppinglist.data.db.entities.ShoppingItem
+import org.abubaker.shoppinglist.databinding.ShoppingItemBinding
 import org.abubaker.shoppinglist.ui.shoppinglist.ShoppingViewModel
 
 class ShoppingItemAdapter(
     var items: List<ShoppingItem>,
     private val viewModel: ShoppingViewModel
-) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+) : RecyclerView.Adapter<ShoppingItemAdapter.ViewHolder>() {
 
-    // ShoppingViewHolder
-    inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    // onCreateViewHolder()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
-        return ShoppingViewHolder(view)
+    // ViewHolder will require 3 Members
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+    inner class ViewHolder(view: ShoppingItemBinding) : RecyclerView.ViewHolder(view.root) {
+
+        val tvName = view.tvName
+        val tvAmount = view.tvAmount
+
+        val ivDelete = view.ivDelete
+        val ivPlus = view.ivPlus
+        val ivMinus = view.ivMinus
+
     }
 
-    // onBindViewHolder()
-    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+
+    // Member 1/3 - Inflate the XML file: shopping_item.xml
+    /**
+     * Inflates the item views which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+
+
+    // onCreateViewHolder()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val binding: ShoppingItemBinding =
+            ShoppingItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    // Member 2/3 - How each item will look like?
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val curShoppingItem = items[position]
 
-        holder.itemView.tvName.text = curShoppingItem.name
-        holder.itemView.tvAmount.text = "${curShoppingItem.amount}"
+        holder.tvName.text = curShoppingItem.name
+        holder.tvAmount.text = "${curShoppingItem.amount}"
 
-        holder.itemView.ivDelete.setOnClickListener {
+        holder.ivDelete.setOnClickListener {
             viewModel.delete(curShoppingItem)
         }
 
-        holder.itemView.ivPlus.setOnClickListener {
+        holder.ivPlus.setOnClickListener {
             curShoppingItem.amount++
             viewModel.upsert(curShoppingItem)
         }
 
-        holder.itemView.ivMinus.setOnClickListener {
+        holder.ivMinus.setOnClickListener {
             if (curShoppingItem.amount > 0) {
                 curShoppingItem.amount--
                 viewModel.upsert(curShoppingItem)
@@ -47,7 +85,10 @@ class ShoppingItemAdapter(
         }
     }
 
-    // Total Number of Rows?
+    // Member 3/3
+    /**
+     * Gets the number of items in the list
+     */
     override fun getItemCount(): Int {
         return items.size
     }
